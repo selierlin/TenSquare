9b5ff2d5741eb30c6100a00fdea5b1b12a671c5e
theliang <theliang@163.com>
2019-07-15 19:55:22 +0800

    修改配置

--- 代码/tensquare_parent/tensquare_eureka/src/main/java/com/tensquare/eureka/EurekaServer.java
+++ 代码/tensquare_parent/tensquare_eureka/src/main/java/com/tensquare/eureka/EurekaServer.java
@@ -1,8 +1,8 @@
 package com.tensquare.eureka;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
